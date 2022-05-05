c97f568f531f6d85dfbbe3fba71402e90bb02e47
sajolbhadra <sajolbhadra@gmail.com>
2022-05-06 01:34:30 +0600

    useInventory hook created

--- src/pages/Home/Items/Items.js
+++ src/pages/Home/Items/Items.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import useInventory from '../../hooks/useInventory';
 
 const Items = () => {
+    const [inventory, setInventory] = useInventory();
+    console.log(inventory)
+    // const { name, img} = inventory;
     return (
         <div>
-            <h1>Items</h1>
+            <h1>Items </h1>
+            {Map.inventory}
         </div>
     );
 };
---
+++ src/pages/hooks/useInventory.js
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+
+const useInventory = () => {
+    const [inventory, setInventory] = useState();
+    useEffect( () => {
+        fetch('http://localhost:5000/item')
+        .then(res => res.json())
+        .then(data => setInventory(data))
+    }, []);
+    return [inventory, setInventory];
+};
+export default useInventory;
